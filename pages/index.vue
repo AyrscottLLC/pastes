faaf4ae63f2999eb20f9e96b916014ee48831271
Jared De Blander <jared@ayrscott.com>
2023-07-05 10:34:08 -0400

    created a custom tailwind css file

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,14 @@
 <template>
     <div>
-        <h1>Greetings</h1>
-        <p>Welcome to pastes.ayrscott.com, a simple place to share the latest and greatest in all things copy pasta 🍝</p>
+        <h1 class="text-4xl pb-4 font-bold">Greetings</h1>
+        <p class="pb-4">Welcome to pastes.ayrscott.com, a simple place to share the latest and greatest in all things copy
+            pasta 🍝</p>
         <form action="/api/new" method="POST">
+            <h2 class="text-2xl font-bold pb-2">Post Title (optional)</h2>
             <input type="text" name="title" placeholder="Title" /><br />
+            <h2 class="text-2xl font-bold pb-2">Paste Text</h2>
             <textarea name="content" placeholder="Content"></textarea><br />
-            <input type="submit" value="Submit" />
+            <input class="btn" type="submit" value="Paste It!" />
         </form>
     </div>
 </template>
