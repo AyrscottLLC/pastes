0e6309f5b7401442bd28e249efee730411d1557f
Jared De Blander <jared@ayrscott.com>
2023-07-05 10:46:15 -0400

    more styling with tailwind

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,17 @@
 <template>
     <div>
-        <h1 class="text-4xl pb-4 font-bold">Greetings</h1>
+        <h1 class="text-4xl pb-4 font-bold text-center">Greetings</h1>
         <p class="pb-4">Welcome to pastes.ayrscott.com, a simple place to share the latest and greatest in all things copy
             pasta 🍝</p>
         <form action="/api/new" method="POST">
             <h2 class="text-2xl font-bold pb-2">Post Title (optional)</h2>
-            <input type="text" name="title" placeholder="Title" /><br />
+            <input type="text" name="title" placeholder="Title"
+                class="block p-2.5 w-full  text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500" /><br />
             <h2 class="text-2xl font-bold pb-2">Paste Text</h2>
-            <textarea name="content" placeholder="Content"></textarea><br />
-            <input class="btn" type="submit" value="Paste It!" />
+            <textarea id="content" rows="4"
+                class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500"
+                placeholder="Paste your text here..."></textarea>
+            <input class="btn mx-auto block mt-8" type="submit" value="Paste It!" />
         </form>
     </div>
 </template>
