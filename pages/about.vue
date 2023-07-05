5cab72dc3f09e4d29c5ac8f26c2665ce403c37fb
Jared De Blander <jared@ayrscott.com>
2023-07-05 10:59:37 -0400

    updated the about page, made links blue

--- pages/about.vue
+++ pages/about.vue
@@ -1,8 +1,18 @@
 <template>
     <div>
-        <h1>About</h1>
-        <p>This is a portfolio project demonstrating competency with Nuxt 3.</p>
-        <p>This project's source code is available on github.</p>
+        <h1 class="text-4xl pb-4 font-bold text-center">About</h1>
+        <p>This is a portfolio project demonstrating full stack competency with a collection of technologies
+            including
+            <a href="https://nuxt.com/">Nuxt JS 3</a>,
+            <a href="https://vuejs.org/">Vue</a>,
+            <a href="https://www.netlify.com/">Netlify</a>,
+            <a href="https://tailwindcss.com/">Tailwind CSS</a>,
+            <a href="https://supabase.com/">Supabase</a>
+            &amp;
+            <a href="https://www.postgresql.org/">PostgreSQL</a>.
+        </p>
+        <p class="mt-4">This project's source code is available on a <a class="text-blue-500"
+                href="https://github.com/AyrscottLLC/pastes">GitHub</a>.</p>
     </div>
 </template>
 
